2dbf75e8be9c48ac2afd3d504957e8cf53d0ca44
Ngoc Minh <tranngocminhcdn@gmail.com>
2020-10-07 10:18:40 +0700

    Update myname.c

--- myname.c
+++ myname.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+  //Hiển thị ba dòng ra màn hình:
+  // Dòng thứ nhất là thông điệp: Xin chao cac ban!
+  // Dòng thứ hai là thông điệp: Toi ten la....
+  // Dòng thứ ba là thông điệp: Lớp...
+  // Chú ý: thông điệp dòng thứ hai và thứ ba phải điền đầy đủ họ tên, lớp sau dấu 3 chấm
+  
+  getchar();
+  return 1;
+}
