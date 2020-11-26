3288477929fbca95620265cb563a5a4cd65c3ef6
thanhduy <72543537+tranthanhduy16052002@users.noreply.github.com>
2020-11-26 15:26:56 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -1,23 +1,20 @@
+#include <stdio.h>
 
 int main (void){
 	
 	int a ;int b;
 	a=3;b=5;
 	
-	printf("a\t"); printf("b\t"); printf("a+b\t"); printf("a-b\t"); printf("a*b\t"); printf("a/b\n");
-	
-	
 	a+b; a-b; a*b; a/b;
-	
-	printf("%d\t",a); printf("%d\t",b);
-	 
-	printf("%d\t",a+b); printf("%d\t",a-b);
-	
-	printf("%d\t",a*b); 
 	
 	double c = 3;
     double d = 5;
-    printf("%g", c/d); 
-	 
+	
+	printf("a\t b\t a+b\t a-b\t a*b\t a/b\n");
+	
+	printf("%d\t %d\t %d\t %d\t %d\t %g",a,b,a+b,a-b,a*b,c/d);
+
+    
 	return 0;
 }
+
