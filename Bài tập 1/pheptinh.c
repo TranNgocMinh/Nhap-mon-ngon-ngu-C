de6122a62ece88ada5e1ddb7f1e2eb089481eeae
nnmv892 <72543416+nnmv892@users.noreply.github.com>
2020-11-26 01:54:45 -0500

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a,b;
+	a = 3;
+	b = 5;
+	int tong, hieu, tich;
+	double thuong;
+	tong = a + b;
+	hieu = a - b;
+	tich = a * b;
+	thuong = (double)a / b;
+	printf("a + b = %d, a - b = %d, a * b = %d, a / b = %0.1f", tong, hieu, tich, thuong);
+	getchar();
+	return 0;
+}
