9dc16876133e53d6dc8bfe59a00cfff479c76a5c
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2020-12-03 10:10:56 +0700

    Create tong.c

---
+++ Bài tập 2/tong.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+  printf("Nhap a = ");
+  scanf("%d",&a);
+  printf("Nhap b = ");
+  scanf("%d",&b);
+  printf("a + b = %d",a + b);
+  return 0;
+}
