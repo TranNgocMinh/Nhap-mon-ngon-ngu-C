f823bf3078411cd18e8bd7e8be996c005dbfcf60
thanhduy <72543537+tranthanhduy16052002@users.noreply.github.com>
2020-12-03 15:23:23 +0700

    Update chieudai.c

--- Bài tập 2/chieudai.c
+++ Bài tập 2/chieudai.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 main(void) {
-  int l,i,f;
+  int l,t,d;
   printf("Nhap do dai can chuyen doi: %d",l);
   scanf("%d", &l);
-  i=l*100;
-  f=i*100;
-  printf("\nDo dai chuyen sang don vi Xentimet la: %d", i);
-  printf("\nDO dai can chuyen sang don vi Milimet la: %d", f);
+  t=l*100;
+  d=t*100;
+  printf("\nDo dai chuyen sang don vi Xentimet la: %d", t);
+  printf("\nDO dai can chuyen sang don vi Milimet la: %d", d);
 }
