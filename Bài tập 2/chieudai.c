b46d268274314fa10c6fa47822116a91f1f4d444
thanhduy <72543537+tranthanhduy16052002@users.noreply.github.com>
2020-12-03 15:02:03 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(void) {
+  int l,i,f;
+  printf("Nhap do dai can chuyen doi: %d",1);
+  scanf("%d", &1);
+  i=l*100;
+  f=i*100;
+  printf("\nDo dai chuyen sang don vi Xentimet la: %d", i);
+  printf("\nDO dai can chuyen sang don vi Milimet la: %d", f);
+}
