d292f4eb78bf3c484b47222e02d3345bd4ddb25f
thanhduy <72543537+tranthanhduy16052002@users.noreply.github.com>
2020-12-03 16:23:17 +0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(void) {
+  int van,toan;
+  printf("Nhap diem van va toan cua mot hoc sinh ");
+  scanf ("%V, %T");
+  scanf ("%DTB");
+  printf("Diem van(V)\tDiem toan(T)\tDien trung binh(DTB)\n");
+  printf ("9\t6\t8");
+
+}
