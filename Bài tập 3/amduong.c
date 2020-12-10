2c46d8432695aafeaba4b3d5a0b4223f7a4a18ee
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2020-12-10 08:51:30 +0700

    Create amduong.c

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 5;
+  //kiem tra a la am hay duong
+  if(a>= 0){
+    printf("%d la so khong am", a);
+  } else{
+    printf("%d la so am", a);
+  }
+
+  //hien thi noi duong
+  printf("%d", a);
+   return 0;
+}
