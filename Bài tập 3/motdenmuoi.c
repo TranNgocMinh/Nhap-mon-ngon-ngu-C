b8d3c557c929affe3512dfabfbc862d712ebf29e
nnmv892 <72543416+nnmv892@users.noreply.github.com>
2020-12-24 03:09:38 -0500

    Update motdenmuoi.c

--- Bài tập 3/motdenmuoi.c
+++ Bài tập 3/motdenmuoi.c
@@ -3,8 +3,8 @@ int main(void){
 	int count;
 	for (count =1; count <=1; count++ ){
 		printf("1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
-	} for (count =1; count <=10; count++ ){
-		printf("12345678910 \n");
+	} for (count =1; count <=1; count++ ){
+		printf("2\n3\n4\n5\n6\n7\n8\n9\n10");
 	}
 	
 }
