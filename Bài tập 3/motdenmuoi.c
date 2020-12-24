c5d582737efee439f102eef2cde7f2ab9a38e8bd
thanhduy <72543537+tranthanhduy16052002@users.noreply.github.com>
2020-12-24 14:36:39 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){ 
+int o;
+printf("tu 1 den 10: \n\n");
+for(o=1; o <=10; o+=1)
+printf("%d\t", o); 
+     int i;
+printf("tu 1 den 10: \n\n");
+for(i=1; i <=10; i+=1)
+printf("%d\n", i);
+}
