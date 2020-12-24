3e13ac7c8df7e0eda931269af9f55f34f3e96a56
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2020-12-24 08:53:01 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	//Khai bao bien
+	int a;
+	//Vong lap
+	for (a = 1; a <= 10; a++)
+{	
+	//Hien thi ra man hinh 10 lan xin chao
+	printf("%d: Xin chao\n", a);
+}
+return 0;
+}
