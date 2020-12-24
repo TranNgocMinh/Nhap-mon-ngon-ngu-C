6e2e0edc5fefc2196825052e48bb4c378660d5bd
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2020-12-24 09:22:52 +0700

    Create 1den10.c

---
+++ Bài tập 3/1den10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	//Khai bao bien
+	int i;
+	//Vong lap tu 1 den 10 tren 1 hang
+	for (i = 1; i <= 10; ++i)
+{	
+		//Hien thi ra man hinh
+		printf("\t %d ", i);
+}
+	//Vong lap tu 1 den 10 tren 10 hang
+	for (i = 1; i <= 10; ++i)
+{	
+		//Hien thi ra man hinh
+		printf(" \t \n %d ", i);
+}
+
+return 0;
+}
