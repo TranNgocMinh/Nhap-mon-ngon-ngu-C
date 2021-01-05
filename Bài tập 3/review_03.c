0d427b638a18df11a419ac82b39f5d1f4cbaa5d1
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2021-01-05 13:14:54 +0700

    Create review_03.c

---
+++ Bài tập 3/review_03.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	//Khai bao bien
+	int i;
+	//Vong lap so le tu 1 den 20 tru so 5, 11, 15
+	for (i = 1; i < 4; i = i + 2)
+{	
+		//Hien thi ra man hinh
+		printf("%d ", i);
+}
+for (i = 7; i <11 ; i = i + 2)
+{	
+		//Hien thi ra man hinh
+		printf("%d ", i);
+}
+for (i = 13; i < 15; i = i + 2)
+{	
+		//Hien thi ra man hinh
+		printf("%d ", i);
+}
+for (i = 17; i < 21; i = i + 2)
+{	
+		//Hien thi ra man hinh
+		printf("%d ", i);
+}
+  
+return 0;
+}
