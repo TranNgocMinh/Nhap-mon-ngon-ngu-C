805b0126e191cc294db7446ec1262408d02d4f13
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2021-01-07 11:09:00 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,19 +2,16 @@
 
 int main()
 {
-//khia bao bien i, j
 int i , j;
-  printf("\tBANG CUU CHUONG\n");
   //Vong lap tu 1 den 10
-for (i = 1; i < 11; i++)
+for (i = 1; i <= 10; i++)
 {
-  //Vong lap nhan tu 1 den 10
-for (j = 1; j < 10; j++)
+for (j = 2; j <= 4; j++)
 {
   //Hien thi ra man hinh ket qua bang cuu chuong ra man hinh
   printf("%d*%d=%d\t", j, i, j * i);
 }
-  printf("\n");
+printf("\n");
 }
 
 return 0;
