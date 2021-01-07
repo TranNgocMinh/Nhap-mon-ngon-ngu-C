33869092738c25b2665271cdd2ccdeb1aa5ba94a
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2021-01-07 11:17:08 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,17 +2,16 @@
 
 int main()
 {
-int i , j;
-  //Vong lap tu 1 den 10
-for (i = 1; i <= 10; i++)
-{
-for (j = 2; j <= 4; j++)
-{
-  //Hien thi ra man hinh ket qua bang cuu chuong ra man hinh
-  printf("%d*%d=%d\t", j, i, j * i);
-}
-printf("\n");
-}
-
+int a , b, c;
+  a = 2; b = 3; c = 4;
+  printf("\nbang 2\n");
+  for(int i = 1; i <=10; i++)
+  printf("\n%d*%d=%d\t",a,i,a*i);
+  printf("\nbang 3\n");
+  for(int i = 1; i <=10; i++)
+  printf("\n%d*%d=%d\t",b,i,b*i);
+  printf("\nbang 4\n");
+  for(int i = 1; i <=10; i++)
+  printf("\n%d*%d=%d\t",c,i,c*i);
 return 0;
 }
