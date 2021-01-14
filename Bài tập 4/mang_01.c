3e2ee6b4cb20d6626f5f88024effe84ba2097371
nguyenminhvuong20002 <72543416+nguyenminhvuong20002@users.noreply.github.com>
2021-01-14 02:43:50 -0500

    Create mang_01.c

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a[4]={3,5,7,2};
+  int i,sum=0;
+
+  for(int i = 0; i < 4; ++i) {
+     printf("%d\n", a[i]);
+     sum +=a[i];
+    
+  }
+  printf("TONG = %d ",sum);
+  return 0;
+}
