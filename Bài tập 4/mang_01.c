ce6ccd4cb33b753843485716fe2285e25e37ef2c
Thanh Nha <74333893+nhachanhdanh@users.noreply.github.com>
2021-01-13 17:31:53 -0800

    Create mang_01.c

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int a[] = {3, 5, 7, 2};
+    //hien thi ra man hinh tu mang
+    printf("%d, %d, %d, %d",3, 5, 7, 2);
+     //hien thi tong day so tren man hinh
+    printf("%d", 3 + 5 + 7 + 2);
+    
+      return 0;
+}
